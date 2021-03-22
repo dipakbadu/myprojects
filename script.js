df0ae4e6ca4f9027a942c42d698bd1f4ac452711
Deepak Badu <dips.badu@gmail.com>
2021-03-22 11:48:51 +0545

    fatch projects from gitlab and display in html page

---
+++ script.js
@@ -0,0 +1,26 @@
+function getData(){
+fetch("https://gitlab.com/api/v4/users/deepakbadu/projects")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Could not reach website.");
+    }
+    return response.json();
+  })
+  .then(data =>{
+    const html = data.map(projects => {
+      return `
+        <tr>
+          <td> ${projects.id}</td>
+          <td>${projects.name}</td>
+          <td> <a href= ${projects.web_url}> ${projects.web_url} </a></td>
+        </tr>`
+    }).join("")
+    document
+    .querySelector("#app")
+    .insertAdjacentHTML('afterbegin', html) 
+  })
+  .catch(err => {
+    return err
+  });
+}
+getData()
